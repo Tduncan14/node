961b85774972159f8b930d8d179f2b474ec9d704
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-12-20 23:31:35 -0600

    keep moving forward

--- fetching-metadata/metadata.js
+++ fetching-metadata/metadata.js
@@ -6,13 +6,18 @@ const file = process.argv[2];
 function printMetadata(file){
 
  try{ const fileStats = fs.statSync(file);
+
+   console.log(process.argv[0],'data');
+   console.log(process.argv[1],'more data');
   console.log(fileStats);
+
+
  }
 
  catch(err){
 
 
-    console.error('the file or directory doesnt exist')
+    console.error('the file or directory doesnt exist',file)
 
 
 
